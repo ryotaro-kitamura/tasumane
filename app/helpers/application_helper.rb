3f2ce010a11b4e616a6099d65cabdcaeeba6b465
ryotaro-kitamura <k.ryoutaro.g@gmail.com>
2019-05-21 12:40:02 +0900

    twitter card

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     twitter_card[:url] = 'https://tasumane.herokuapp.com/'
     twitter_card[:title] = '簡易的なタスク記録アプリ「タスマネ」'
     twitter_card[:description] = 'タスマネであなたが頑張ったタスクを記録してシェアしよう'
-    twitter_card[:image] = 'app/assets/images/tasumane.png'
+    twitter_card[:image] = 'https://tasumane.herokuapp.com/app/assets/images/tasumane.png'
     twitter_card[:card] = 'summary_large_image'
     twitter_card[:site] = '@agepiitweets'
     twitter_card
